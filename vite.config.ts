1cd723b38a25adf5b0432fc0b6e955ea2c6a7cbe
박병훈 <leafloveu@naver.com>
2025-11-25 19:19:34 +0900

    feat: allowedHosts 추가

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    allowedHosts: ['bhoon.iptime.org'],
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
